63ad2546d98aa0e47d95e19dca6da2096f9aad6d
Aditya Prayoga <adityaprayoga32@gmail.com>
2024-06-03 16:58:38 +0700

    cors

--- controller/authToken.js
+++ controller/authToken.js
@@ -5,6 +5,7 @@ export const authToken = async(req, res)=>{
     try{
         const authToken = req.cookies.authToken;
         if(!authToken) return res.sendStatus(401);
+        console.log("sudah lewat 401 di authcontroller")
         const user = await User.findOne({
             where:{
                 auth_token:authToken
@@ -13,6 +14,7 @@ export const authToken = async(req, res)=>{
         if(!user.auth_token) return res.sendStatus(403);
         else jwt.verify(authToken, process.env.AUTH_TOKEN_SECRET,(err, decoded)=>{
             if(err) return res.sendStatus(403);
+            console.log("sudah lewat 403 ke dua di controller")
             const userPayload = {
                 id: user.id,
                 nama: user.nama,
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cookieParser());
-app.use(cors({credentials: true, origin:'*' }));
+app.use(cors({credentials: true, origin:'http://localhost:3000' }));
 app.use(express.json());
 app.use(UserRoute);
 
